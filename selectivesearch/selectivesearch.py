eae41175578933f7aa169cc2fdd4d9db5070e912
lj1995513 <lijing.coder@qq.com>
2017-08-13 20:16:25 +0800

    python3 support

--- selectivesearch/selectivesearch.py
+++ selectivesearch/selectivesearch.py
@@ -6,7 +6,7 @@ import skimage.transform
 import skimage.util
 import skimage.segmentation
 import numpy
-
+from functools import cmp_to_key
 
 # "Selective Search for Object Recognition" by J.R.R. Uijlings et al.
 #
@@ -203,7 +203,7 @@ def _extract_neighbours(regions):
             return True
         return False
 
-    R = regions.items()
+    R = list(regions.items())
     neighbours = []
     for cur, a in enumerate(R[:-1]):
         for b in R[cur + 1:]:
@@ -282,7 +282,8 @@ def selective_search(
     while S != {}:
 
         # get highest similarity
-        i, j = sorted(S.items(), cmp=lambda a, b: cmp(a[1], b[1]))[-1][0]
+        i, j = sorted(S.items(), key=cmp_to_key(
+            lambda a, b: a[1] - b[1]))[-1][0]
 
         # merge corresponding regions
         t = max(R.keys()) + 1.0
